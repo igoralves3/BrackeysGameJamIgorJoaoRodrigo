fdd38f795af284a0115d26609b0e37e4c56f815c
JoaoPioner <jvpioner191@gmail.com>
2025-08-27 14:14:11 -0300

    Sound manager criado

---
+++ bragamejam2025.2_guildmaster/Scenes/SoundManager/sound_manager.gd
@@ -0,0 +1,38 @@
+extends Node
+
+@onready var BGMusicPlaylist: Array = ["res://Resources/Sounds/BGMusic1.wav", "res://Resources/Sounds/BGMusic2.wav", "res://Resources/Sounds/BGMusic3.wav", "res://Resources/Sounds/BGMusic4.wav", "res://Resources/Sounds/BGMusic5.wav"]
+@onready var FailPlaylist: Array = ["res://Resources/Sounds/fail1.wav", "res://Resources/Sounds/fail2.wav", "res://Resources/Sounds/fail3.wav"]
+@onready var VictoryPlaylist: Array = ["res://Resources/Sounds/victory1.wav", "res://Resources/Sounds/victory2.wav"]
+@onready var ButtonPlayList: Array = ["res://Resources/Sounds/Button1.wav", "res://Resources/Sounds/Button2.wav", "res://Resources/Sounds/Button3.wav"]
+
+
+func _ready():
+	pickBGMusic(randi() % 5)
+
+func pickBGMusic(index: int):
+	$BG.stop()
+	print(index)
+	print(BGMusicPlaylist[index])
+	$BG.stream = load(BGMusicPlaylist[index])
+	$BG.play()
+
+func pickFailMusic(index: int):
+	$SFX.stop()
+	print(index)
+	print(FailPlaylist[index])
+	$SFX.stream = load(FailPlaylist[index])
+	$SFX.play()
+
+func pickVictoryMusic(index: int):
+	$SFX.stop()
+	print(index)
+	print(VictoryPlaylist[index])
+	$SFX.stream = load(VictoryPlaylist[index])
+	$SFX.play()
+
+func pickButtonSFX(index: int):
+	$SFX.stop()
+	print(index)
+	print(ButtonPlayList[index])
+	$SFX.stream = load(ButtonPlayList[index])
+	$SFX.play()
