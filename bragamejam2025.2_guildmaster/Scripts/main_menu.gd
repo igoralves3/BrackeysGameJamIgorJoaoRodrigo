575d362189442a8939ab6c2149e9a85e6a2341f9
lucasys <61033136+luukida@users.noreply.github.com>
2025-08-30 18:54:23 -0300

    Main menu and Day Cycle scenes
    Added animations for main menu
    Created new day cycle scene

--- bragamejam2025.2_guildmaster/Scripts/main_menu.gd
+++ bragamejam2025.2_guildmaster/Scripts/main_menu.gd
@@ -2,44 +2,36 @@ extends Control
 
 const scenetoLoad = preload("res://Scenes/Components/Main.tscn")
 
-@onready var playerButtonContainer = $MainMenuContainer/MainMenuVBoxContainer/PlayButtonContainer/PlayGameButton
-@onready var guildNameInputContainer = $MainMenuContainer/MainMenuVBoxContainer/PlayButtonContainer/InputFieldContainer
-
-@onready var menuContainer = $MainMenuContainer
-
-func _process(delta: float) -> void:
-	while (menuContainer.modulate.a < 255):
-		menuContainer.modulate.a += 0.15 * delta
-		if get_tree():
-			await get_tree().create_timer(1).timeout
+@onready var play_game_button: TextureButton = $MainMenuContainer/MainMenuVBoxContainer/VBoxContainer/PlayGameButton
+@onready var v_box_container: VBoxContainer = $MainMenuContainer/MainMenuVBoxContainer/VBoxContainer
+@onready var line_edit: LineEdit = $MainMenuContainer/MainMenuVBoxContainer/GuildNameBoxContainer/LineEdit
 
 func _on_play_game_button_pressed() -> void:
 	
-	playerButtonContainer.visible = false
-	guildNameInputContainer.visible = true
-	
-	#SceneTransition.transition()
-	#SoundManager.pickButtonSFX(randi() % 3)
-	#await SceneTransition.on_transition_finished
-	#get_tree().change_scene_to_packed(scenetoLoad)
-
-
-func _on_play_game_button_mouse_entered() -> void:
-	$MainMenuContainer/MainMenuVBoxContainer/PlayButtonContainer/PlayGameButton/Label.self_modulate = Color("311006")
-
-
-func _on_play_game_button_mouse_exited() -> void:
-	$MainMenuContainer/MainMenuVBoxContainer/PlayButtonContainer/PlayGameButton/Label.self_modulate = Color("943f23")
+	if line_edit.text == "":
+		line_edit.placeholder_text = "Type a name!"
+	else:
+		if ("guild" in line_edit.text) or ("clan" in line_edit.text) or ("Guild" in line_edit.text) or ("Clan" in line_edit.text):
+			Globals.guildName = line_edit.text
+		else:
+			Globals.guildName = line_edit.text + " Guild"
+		
+		SceneTransition.transition()
+		SoundManager.pickButtonSFX(randi() % 3)
+		await SceneTransition.on_transition_finished
+		get_tree().change_scene_to_packed(scenetoLoad)
 
 
 func _on_line_edit_text_submitted(new_text: String) -> void:
-	
-	if ("guild" in new_text) or ("clan" in new_text) or ("Guild" in new_text) or ("Clan" in new_text):
-		Globals.guildName = new_text
+	if line_edit.text == "":
+		line_edit.placeholder_text = "Type a name!"
 	else:
-		Globals.guildName = new_text + " Guild"
-	
-	SceneTransition.transition()
-	SoundManager.pickButtonSFX(randi() % 3)
-	await SceneTransition.on_transition_finished
-	get_tree().change_scene_to_packed(scenetoLoad)
+		if ("guild" in line_edit.text) or ("clan" in line_edit.text) or ("Guild" in line_edit.text) or ("Clan" in line_edit.text):
+			Globals.guildName = line_edit.text
+		else:
+			Globals.guildName = line_edit.text + " Guild"
+		
+		SceneTransition.transition()
+		SoundManager.pickButtonSFX(randi() % 3)
+		await SceneTransition.on_transition_finished
+		get_tree().change_scene_to_packed(scenetoLoad)
